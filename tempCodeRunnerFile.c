04eff988adda92182c10e5d1b0b7ea15279b6c4a
zxhyJack <zhuhongjie0318@163.com>
2021-03-30 19:38:23 +0800

    del temp.c

--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-
-            // {
-            //     r1 = (int)rand() % NP;
-            //     r2 = (int)rand() % NP;
-            //     r3 = (int)rand() % NP;
-            // } while (r1 == i || r2 == i || r3 == i || r1 == r2 || r2 == r3 || r1 == r3);
