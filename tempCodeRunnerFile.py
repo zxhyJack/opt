5c268b089e6727f2737c27ab074fbda30f32967f
zxhyJack <zhuhongjie0318@163.com>
2021-04-12 15:55:30 +0800

    删除文件 tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-self.ns1 += 1
-                        elif strategy == 2:
-                            self.ns2 += 1
-                    else:
-                        if strategy == 1:
-                            self.nf1 += 1
-                        elif strategy == 2:
-                            
